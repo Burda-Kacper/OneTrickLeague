d6d7bd7530cbf8201ee647f60a13fd0dc654ce0b
Burda-Kacper <burda.kacper.kontakt@gmail.com>
2021-11-08 23:28:19 +0100

    User entity and basic layout

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageController extends AbstractController
+{
+    public function homepage(): Response
+    {
+        return $this->render('homepage/homepage.html.twig');
+    }
+}
